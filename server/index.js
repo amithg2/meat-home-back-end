446c40a30859604978e9b27b420073433600f641
amithg2 <amitadi91@gmail.com>
2021-12-19 21:53:49 +0200

    small chaanges

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 const express = require("express");
 const resrvationsRoutes = require("./routes/reservationsRoutes");
 const guest = require("./routes/guest");
